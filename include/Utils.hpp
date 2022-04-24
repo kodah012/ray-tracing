23d73cc14ec17370f66c0dbf16180d14f4c9b7a5
kodah <koadahoakodah@gmail.com>
2022-04-23 23:22:47 -0400

    Add functions for generating random numbers

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 namespace Math {
 
@@ -11,6 +12,18 @@ namespace Math {
 
   inline double degreesToRadians(double degrees) {
     return degrees * pi / 180.0;
+  }
+
+  // Returns a random real number in [0, 1)
+  inline double randomDouble() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+  }
+
+  // Returns a random real number in [min, max)
+  inline double randomDouble(double min, double max) {
+    return min + (max - min) * randomDouble();
   }
 
 }
